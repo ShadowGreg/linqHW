5b33653c8886ba1155668188a4d3045ee9034f69
Greg Osetrov <shadow-greg@yandex.ru>
2024-07-02 10:53:03 +0300

    готово

---
+++ linqHW/ValidAnagram.cs
@@ -0,0 +1,22 @@
+﻿namespace linqHW
+{
+    public class ValidAnagram
+    {
+        public static void DoAnagram()
+        {
+            string anagram = "anagram", nagaram = "nagaram";
+            bool isAnagram = true;
+
+
+            bool isAnagramTo = GetAnswer(anagram, nagaram);
+
+            Console.WriteLine(Equals(isAnagram, isAnagramTo));
+
+        }
+
+        private static bool GetAnswer(string anagram, string nagaram)
+        {
+            return anagram.Length == nagaram.Length && anagram.OrderBy(x => x).SequenceEqual(nagaram.OrderBy(x => x));
+        }
+    }
+}
