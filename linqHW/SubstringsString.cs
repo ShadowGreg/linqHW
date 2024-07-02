5e594022c4106bc5ad2986a168dda79f0e27b16b
Greg <shadow-greg@yandex.ru>
2024-07-02 06:19:19 +0300

    start

--- linqHW/SubstringsString.cs
+++ linqHW/SubstringsString.cs
@@ -1,7 +1,31 @@
+using ConsoleTableExt;
+
 namespace linqHW
 {
     public class SubstringsString
     {
-        
+        private const string Text = "abcd";
+
+        public static void DoSubstring()
+        {
+            List<string> substrings = Text
+                .Select(c => c.ToString())
+                .ToList();
+            List<string> newSubstring = Text
+                .Take(Text.Length - 1)
+                .Select(c=>c.ToString())
+                .ToList();
+            
+            
+            
+            
+            
+            
+            ConsoleTableBuilder
+                .From(substrings)
+                .WithTitle("Words")
+                .ExportAndWriteLine();
+
+        }
     }
 }
