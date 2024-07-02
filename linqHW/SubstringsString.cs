645412ba18879bcc092b82378bbd65a8a07cb300
Greg Osetrov <shadow-greg@yandex.ru>
2024-07-02 10:36:47 +0300

    решила ide

--- linqHW/SubstringsString.cs
+++ linqHW/SubstringsString.cs
@@ -11,19 +11,26 @@ namespace linqHW
             var expected = new List<string> { "a", "b", "c", "d", "ab", "bc", "cd", "abc", "bcd", "abcd" };
             expected.Sort();
 
-            
             List<string> endSubstrings = new List<string>();
-            // List<string> endSubstrings = Text.Select(c => c.ToString()).ToList();
 
-            for (int i = 0; i < Text.Length; i++)
-            {
-                for (int j = 1; j <= Text.Length - i; j++)
-                {
-                    var substring = Text.Substring(i, j);
-                    endSubstrings.Add(substring);
-                }
-            }
+            // for (int i = 0; i < Text.Length; i++)
+            // {
+            //     for (int j = 1; j <= Text.Length - i; j++)
+            //     {
+            //         string substring = Text.Substring(i, j);
+            //         endSubstrings.Add(substring);
+            //     }
+            // }
 
+            List<int> countCollection = GetCountCollection(Text);
+
+
+            endSubstrings = Enumerable.Range(0, Text.Length)
+                .SelectMany(i => Enumerable.Range(1, Text.Length - i)
+                    .Select(j => Text.Substring(i, j))).ToList();
+
+            Console.Clear();
+            Console.WriteLine();
             var sorted = endSubstrings.OrderBy(word => word).ToList();
 
             Console.WriteLine("Equals returns: " + sorted.SequenceEqual(expected));
@@ -31,6 +38,12 @@ namespace linqHW
                 .From(endSubstrings)
                 .WithTitle("Words")
                 .ExportAndWriteLine();
+        }
+
+        private static List<int> GetCountCollection(string text)
+        {
+            int i = 0;
+            return text.Select(ch => i++).ToList();
         }
     }
 }
