a5bfce49d98bcfd2965a78e757bd4af675bad05b
Greg Osetrov <shadow-greg@yandex.ru>
2024-07-02 08:50:16 +0300

    ага заработало через циклы

--- linqHW/SubstringsString.cs
+++ linqHW/SubstringsString.cs
@@ -8,24 +8,24 @@ namespace linqHW
 
         public static void DoSubstring()
         {
-            List<string> substrings = Text
-                .Select(c => c.ToString())
-                .ToList();
-            List<string> newSubstring = Text
-                .Take(Text.Length - 1)
-                .Select(c=>c.ToString())
-                .ToList();
-            
-            
-            
-            
-            
-            
+            List<string> endSubstrings = new List<string>();
+            // List<string> endSubstrings = Text.Select(c => c.ToString()).ToList();
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                for (int j = 1; j <= Text.Length - i; j++)
+                {
+                    var substring = Text.Substring(i, j);
+                    endSubstrings.Add(substring);
+                }
+            }
+
+            Console.WriteLine("Equals returns: " + Equals(endSubstrings,
+                new List<string> { "a", "b", "c", "d", "ab", "bc", "cd", "abc", "bcd", "abcd" }));
             ConsoleTableBuilder
-                .From(substrings)
+                .From(endSubstrings)
                 .WithTitle("Words")
                 .ExportAndWriteLine();
-
         }
     }
 }
