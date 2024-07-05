11bb2016540ef36df5af9e579afa4661f1855126
Greg Osetrov <shadow-greg@yandex.ru>
2024-07-05 10:15:14 +0300

    сделал что бы работало для разного количества букв, но есть до сих пор проблема в том что оно будет убирать дублированные буквы

--- linqHW/PermutationsGivenString.cs
+++ linqHW/PermutationsGivenString.cs
@@ -15,6 +15,14 @@
                 .Select(tuple => $"{tuple.num}{tuple.ch}")
                 .Where(str => str.GroupBy(c => c).All(g => g.Count() == 1)) 
                 .ToHashSet().ToList();
+            
+            
+            var actualNew = given
+                .Aggregate(new List<string> { "" }, (acc, _) =>
+                    acc.SelectMany(prefix => given, (prefix, ch) => $"{prefix}{ch}")
+                        .Where(str => str.GroupBy(c => c).All(g => g.Count() == 1))
+                        .ToList())
+                .ToHashSet();
               
             actual.Sort();
             expected.Sort();
