7c40ec9ac666b91a599733d4adaee38089d67cd8
Greg Osetrov <shadow-greg@yandex.ru>
2024-07-05 09:55:45 +0300

    решил задачку

--- linqHW/PermutationsGivenString.cs
+++ linqHW/PermutationsGivenString.cs
@@ -4,39 +4,21 @@
     {
         public static void PermutationsGivenStringRun()
         {
-            List<string> _expected = ["ABC", "ACB", "BAC", "BCA", "CBA", "CAB"];
-            const string Given = "ABC";
-            const string Given1 = "ABC";
-            List<string> actual = new List<string>();
+            List<string> expected = ["ABC", "ACB", "BAC", "BCA", "CBA", "CAB"];
+            const string given = "ABC";
+           
 
-            /*Enumerable
-            Foreach
-            ElementAt
-            Range*/
-
-            var permutations = Given
-                .SelectMany(num => Given1, (num, ch) => (num, ch));
-                // .Select("{0}{1}", (num, ch) => $"{num}{ch}");
-            
-            
-            //Enumerable
-            //     .Range(0,Given.Length)
-            //     .
-            // Given.ElementAt(1);
-            // Enumerable
-            // .Range(0, Given.Length);
-            // .SelectMany(i => Enumerable
-            //     .Range(0, Given.Length - i)
-            //     .Select(j => Given.Substring(i, j + 1)));
-
-
-            // foreach (var permutation in permutations)
-            // {
-            //     actual.Add(permutation);
-            // }
-
+            var actual = given
+                .SelectMany(num => given, (num, ch) => (num, ch))
+                .Select(tuple => $"{tuple.num}{tuple.ch}")
+                .SelectMany(num => given, (num, ch) => (num, ch))
+                .Select(tuple => $"{tuple.num}{tuple.ch}")
+                .Where(str => str.GroupBy(c => c).All(g => g.Count() == 1)) 
+                .ToHashSet().ToList();
+              
             actual.Sort();
-            Console.WriteLine(actual.SequenceEqual(_expected));
+            expected.Sort();
+            Console.WriteLine(actual.SequenceEqual(expected));
         }
     }
 }
